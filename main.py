ed57e0d624fe940d051154c33cac8fd12f858cad
Galacticboreas <ivanovvolgdonbor@mail.ru>
2023-11-23 23:20:15 +0300

    notebook test

--- main.py
+++ main.py
@@ -23,17 +23,26 @@ search_excpression = os.getenv('search_excpression')
 if __name__ == '__main__':
     with pd.ExcelFile(excel_file_path + excel_file_name) as xls:
         df_main = pd.read_excel(xls, sheet1)
-df_main = df_main[df_main[coll_name1].str.contains(search_excpression)]
-df_main.fillna(0, inplace=True)
+    df_main = df_main[df_main[coll_name1].str.contains(search_excpression)]
+    df_main.fillna(0, inplace=True)
+    df_main['Заказ на сборку'] = df_main['Заказ на сборку'].apply(lambda x: x[43:47]) + df_main['Заказ на сборку'].apply(lambda x: x[29:33])
 
-with pd.ExcelFile(excel_file_path + excel_file_name) as xls:
+    with pd.ExcelFile(excel_file_path + excel_file_name) as xls:
         df1 = pd.read_excel(xls, sheet2)
-df1 = df1[df1[coll_name1].str.contains(search_excpression)]
-df1.fillna(0, inplace=True)
+    df1 = df1[df1[coll_name1].str.contains(search_excpression)]
+    df1.fillna(0, inplace=True)
+    df1['Заказ на сборку'] = df1['Заказ на сборку'].apply(lambda x: x[43:47]) + df1['Заказ на сборку'].apply(lambda x: x[29:33])
 
-df_merge = pd.merge(df_main, df1, how='left')
-df_merge.fillna(0, inplace=True)
+    df_main = pd.merge(df_main, df1, how='left')
+    df_main.fillna(0, inplace=True)
 
-result = df_merge.to_json(orient='table')
-parser = loads(result)
-result_end = dumps(parser, indent=4, ensure_ascii=False)
+    with pd.ExcelFile(excel_file_path + excel_file_name) as xls:
+        df2 = pd.read_excel(xls, sheet3)
+    pattern = r"[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][ ][о][т][ ][0-9][0-9][.][0-9][0-9][.][0-9][0-9][0-9][0-9]"
+    df2 = df2[df2['Заказ на сборку'].str.fullmatch(pattern)]
+    df2['Заказ на сборку'] = df2['Заказ на сборку'].apply(lambda x: x[len(x)-4:len(x)]) + df2['Заказ на сборку'].apply(lambda x: x[len(x)-18:len(x)-39])
+
+    df_main = pd.concat([df_main, df2])
+
+    df_main.to_excel('data/result.xlsx')
+
