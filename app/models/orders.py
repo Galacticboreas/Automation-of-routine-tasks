12cb60037e2e63bfe0aadfebc32a36471e733b86
Galacticboreas <ivanovvolgdonbor@mail.ru>
2023-11-26 02:14:47 +0300

    models Order uniq=False

--- app/models/orders.py
+++ app/models/orders.py
@@ -7,11 +7,11 @@ class Base(DeclarativeBase): pass
 class Order(Base):
     __tablename__= 'order'
     id = Column(Integer, primary_key=True)
-    composite_key = Column(String(8), nullable=False, index=True, unique=True)
+    composite_key = Column(String(8), nullable=False, index=True)
     order_date = Column(DateTime())
     order_main = Column(String(), nullable=False)
     order_number = Column(String(4), nullable=False)
-    nomenclature_name = Column(String(), default='Не задано')
+    nomenclature_name = Column(String())
     oredered = Column(Integer(), default=0)
     released = Column(Integer(), default=0)
     remains_to_release = Column(Integer(), default=0)
