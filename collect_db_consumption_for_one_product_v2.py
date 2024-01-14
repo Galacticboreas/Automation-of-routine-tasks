d6cacc577cf874a7bfa34ea9b7d1ba5916b26edb
Galacticboreas <ivanovvolgdonbor@mail.ru>
2024-01-14 21:10:09 +0300

    collecting data from multiple files

--- collect_db_consumption_for_one_product_v2.py
+++ collect_db_consumption_for_one_product_v2.py
@@ -11,85 +11,163 @@ config.read('settings.ini', encoding='utf8')
 
 exl_file_dir = config['DEFAULT']['Path_dir']
 exl_data_dir = config['DEFAULT']['Path_data']
-exl_file_name = config['DEFAULT']['File_name_orders1']
+File_name_orders1 = config['DEFAULT']['File_name_orders1']
+File_name_orders2 = config['DEFAULT']['File_name_orders2']
+File_name_source_db_1 = config['DEFAULT']['File_name_source_db_1']
+File_name_source_db_2 = config['DEFAULT']['File_name_source_db_2']
 sheet_main = config['DEFAULT']['Sheet_main']
-exl_file = exl_file_dir + exl_data_dir + exl_file_name
+Forecast = config['Sheet.name']['Forecast']
+Not_deploy = config['Sheet.name']['Not_deploy']
+Deploy = config['Sheet.name']['Deploy']
+Sheet_all_categories = config['Sheet.name']['Sheet_all_categories']
+Sheet_sheet_material = config['Sheet.name']['Sheet_sheet_material']
 
+# Пути до файлов с заказами
+orders_files = [
+    File_name_orders1,
+    File_name_orders2,
+]
 
-workbook = load_workbook(
-    filename=exl_file,
-    read_only=True,
-    data_only=True
-    )
+# Данные по заказам на производство
+ordered_data_file1 = defaultdict(list)
+ordered_data_file2 = defaultdict(list)
+data_orders = [
+    ordered_data_file1,
+    ordered_data_file2,
+]
 
-sheet = workbook[sheet_main]
-ordered_data = defaultdict(list)
 extractor = ArticleExtractor()
-
-for value in sheet.iter_rows(min_row=7,
-                             max_col=7,
-                             values_only=True):
-    key = value[3]
-    name = value[4]
-    ordered = value[5]
-    article = extractor.get_article(name)
-    if not ordered_data.get(key):
-        ordered_data[key].append(
-            [
-                name,
-                article,
-                ordered
-            ]
+ordered_data_result = []
+for _ in range(len(orders_files)):
+    exl_file = exl_file_dir + exl_data_dir + orders_files[_]
+    workbook = load_workbook(
+        filename=exl_file,
+        read_only=True,
+        data_only=True
         )
 
-sheet_name_categ = config['Sheet.name']['Sheet_all_categories']
-sheet_name_status = config['Sheet.name']['Forecast']
-sheet_name = sheet_name_categ + " " + sheet_name_status
+    sheet = workbook[sheet_main]
+    ordered_data = data_orders[_]
 
-exl_file_name = config['DEFAULT']['File_name_source_db_1']
-exl_file = exl_file_dir + exl_data_dir + exl_file_name
+    for value in sheet.iter_rows(min_row=7,
+                                 max_col=7,
+                                 values_only=True):
+        key = value[3]       # Полный номер заказа на производство
+        name = value[4]      # Наименование изделия мебели
+        ordered = value[5]   # Количество изделий в заказе
+        article = extractor.get_article(name)   # Артикул изделия мебели
+        if not ordered_data.get(key):
+            ordered_data[key].append(
+                [
+                    name,
+                    article,
+                    ordered
+                ]
+            )
+    ordered_data_result.append(ordered_data)
 
-workbook = load_workbook(
-    filename=exl_file,
-    read_only=True,
-    data_only=True
+# Названия листов с данными по материалам из выгрузки
+sheets = [
+    Sheet_all_categories + " " + Forecast,
+    Sheet_sheet_material + " " + Forecast,
+]
+
+material_files = [
+    File_name_source_db_1,
+    File_name_source_db_2,
+]
+
+materials = []   # Собираем данные по расходу материала на 1 изделие
+
+for _ in range(len(material_files)):
+    exl_file = exl_file_dir + exl_data_dir + material_files[_]
+    workbook = load_workbook(
+        filename=exl_file,
+        read_only=True,
+        data_only=True,
     )
+    for i in range(len(sheets)):
+        sheet = workbook[sheets[i]]
 
-sheet = workbook[sheet_name]
+        for value in sheet.iter_rows(min_row=2, values_only=True):
+            key = value[1]                # Полный номер заказа на производство
+            material_code = value[3]      # Код материала
+            material_article = value[4]   # Артикул материала
+            material_name = value[5]      # Наименованме материала
+            # # Количество материала в заказе
+            material_amount = value[7] if value[7] else 0
 
-materials = []
+            ordered_data = ordered_data_result[_]
+            if ordered_data.get(key):
+                furniture_name = ordered_data[key][0][0]
+                furniture_article = ordered_data[key][0][1]
+                cons_temp = ordered_data[key][0][2]
+                consumption_per_order = cons_temp if cons_temp else 0
+                try:
+                    consumption_per_1_product = material_amount / consumption_per_order
+                except ZeroDivisionError:
+                    consumption_per_1_product = 0
+                material = OrderData(
+                    furniture_article=furniture_article,
+                    furniture_name=furniture_name,
+                    material_code=material_code,
+                    material_article=material_article,
+                    material_name=material_name,
+                    consumption_per_1_product=consumption_per_1_product
+                )
+                materials.append(material)
 
-for value in sheet.iter_rows(min_row=2,
-                             values_only=True):
-    key = value[1]
-    material_code = value[3]
-    material_article = value[4]
-    material_name = value[5]
-    material_amount = value[7] if value[7] else 0
+# Названия листов с данными по материалам из выгрузки
+sheets = [
+    Sheet_all_categories + " " + Not_deploy,
+    Sheet_all_categories + " " + Deploy,
+    Sheet_sheet_material + " " + Not_deploy,
+    Sheet_sheet_material + " " + Deploy,
+]
 
-    if ordered_data.get(key):
-        furniture_article = extractor.get_article(ordered_data[key][0][1])
-        furniture_name = ordered_data[key][0][0]
-        consumption_per_order = ordered_data[key][0][2] if ordered_data[key][0][2] else 0
-        try:
-            consumption_per_1_product = material_amount / consumption_per_order
-        except ZeroDivisionError:
-            consumption_per_1_product = 0
-        material = OrderData(
-            furniture_article=furniture_article,
-            furniture_name=furniture_name,
-            material_code=material_code,
-            material_article=material_article,
-            material_name=material_name,
-            consumption_per_1_product=consumption_per_1_product
-        )
-        materials.append(material)
-len(materials)
+for _ in range(len(material_files)):
+    exl_file = exl_file_dir + exl_data_dir + material_files[_]
+    workbook = load_workbook(
+        filename=exl_file,
+        read_only=True,
+        data_only=True,
+    )
+    for i in range(len(sheets)):
+        sheet = workbook[sheets[i]]
 
+        for value in sheet.iter_rows(min_row=2, values_only=True):
+            key = value[1]                # Полный номер заказа на производство
+            material_code = value[3]      # Код материала
+            material_article = value[4]   # Артикул материала
+            material_name = value[5]      # Наименованме материала
+            # # Количество материала в заказе
+            material_amount = value[7] if value[7] else 0
+
+            ordered_data = ordered_data_result[_]
+            if ordered_data.get(key):
+                furniture_name = ordered_data[key][0][0]
+                furniture_article = ordered_data[key][0][1]
+                cons_temp = ordered_data[key][0][2]
+                consumption_per_order = cons_temp if cons_temp else 0
+                try:
+                    consumption_per_1_product = material_amount / consumption_per_order
+                except ZeroDivisionError:
+                    consumption_per_1_product = 0
+                material = OrderData(
+                    furniture_article=furniture_article,
+                    furniture_name=furniture_name,
+                    material_code=material_code,
+                    material_article=material_article,
+                    material_name=material_name,
+                    consumption_per_1_product=consumption_per_1_product
+                )
+                materials.append(material)
+
+# Файл с результатами расчетов расхода на одно изделие
 workbook = Workbook()
 
 sheet = workbook.active
-sheet.title = sheet_name
+sheet.title = 'Расход на 1 изделие'
 
 sheet.append([
     "Артикул",
