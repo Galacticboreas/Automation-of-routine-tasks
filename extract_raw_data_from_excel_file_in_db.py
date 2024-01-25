ee83178b8d784cd4859a44e7c01fd88841e51491
Galacticboreas <ivanovvolgdonbor@mail.ru>
2024-01-25 09:34:07 +0300

    settings flake8

--- extract_raw_data_from_excel_file_in_db.py
+++ extract_raw_data_from_excel_file_in_db.py
@@ -3,5 +3,5 @@ from app import ReportSettingsOrders
 config = ReportSettingsOrders()
 
 if __name__ == '__main__':
-    if config.log_report:
-        print(config.log_report)
+    print(config.source_file)
+    print(config.report_file)
