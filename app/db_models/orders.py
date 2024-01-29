77e968f924f4730af5dd7e55db370d98bbb08c66
Galacticboreas <ivanovvolgdonbor@mail.ru>
2024-01-29 15:51:05 +0300

    func impot data to db

--- app/db_models/orders.py
+++ app/db_models/orders.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass
-from datetime import datetime
 from enum import Enum
 
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Float
+from sqlalchemy import Column, Float, ForeignKey, Integer, String
 from sqlalchemy.orm import DeclarativeBase, relationship
 
 
@@ -45,13 +44,13 @@ class OrderRowData(Base):
     released = Column(Integer(), default=0)
     remains_to_release = Column(Integer(), default=0)
     release_of_assemblykits = relationship("ReleaseOfAssemblyKitsRowData",
-                                         uselist=False,
-                                         back_populates="order",
-                                         cascade="all, delete")
+                                           uselist=False,
+                                           back_populates="order",
+                                           cascade="all, delete")
     description_main_order = relationship("DescriptionMainOrderRowData",
-                                        uselist=False,
-                                        back_populates='order',
-                                        cascade="all, delete")
+                                          uselist=False,
+                                          back_populates='order',
+                                          cascade="all, delete")
     descriptions = relationship("DescriptionAdditionalOrderRowData",
                                 back_populates='order')
     monitor_for_work_center = relationship("JobMonitorForWorkCenters",
@@ -121,3 +120,63 @@ class DescriptionAdditionalOrderRowData(Base):
     comment = Column(String())
     order_id = Column(Integer(), ForeignKey("orders_row_data.id"))
     order = relationship("OrderRowData", back_populates="descriptions")
+
+
+def import_data_to_db_production_orders(orders_data: dict, session: object)-> object:
+    """Функция импорта данных из словаря данных о заказаз на производство
+    в таблицы БД.
+
+    Args:
+        orders_data (dict): [заказы на производство]
+        session (object): sqlalchemy session
+
+    Returns:
+        session: sqlalchemy session
+    """
+    for key in orders_data:
+        if not (key == 'ErrorLog'):
+            # Основной заказ на производство
+            order = OrderRowData()
+            order.composite_key = key
+            order.full_order_number = orders_data[key]['moving_sets_of_furniture']['full_order_number']
+            order.furniture_name = orders_data[key]['moving_sets_of_furniture']['furniture_name']
+            order.furniture_article = orders_data[key]['moving_sets_of_furniture']['furniture_article']
+            order.ordered = orders_data[key]['moving_sets_of_furniture']['ordered']
+            order.released = orders_data[key]['moving_sets_of_furniture']['released']
+            order.remains_to_release = orders_data[key]['moving_sets_of_furniture']['remains_to_release']
+
+            # Перемещение комплектов мебели (данные учета)
+            if orders_data[key].get("release_of_assembly_kits"):
+                releaseassemblykits = ReleaseOfAssemblyKitsRowData()
+                releaseassemblykits.cutting_shop_for_assembly = orders_data[key]['release_of_assembly_kits']['cutting_workshop_for_assembly']
+                releaseassemblykits.cutting_shop_for_painting = orders_data[key]['release_of_assembly_kits']['cutting_workshop_for_painting']
+                releaseassemblykits.paint_shop_for_assembly = orders_data[key]['release_of_assembly_kits']['paint_shop_for_assembly']
+                releaseassemblykits.assembly_shop = orders_data[key]['release_of_assembly_kits']['assembly_shop']
+                releaseassemblykits.order = order
+            else:
+                releaseassemblykits = ReleaseOfAssemblyKitsRowData()
+                releaseassemblykits.cutting_shop_for_assembly = 0
+                releaseassemblykits.cutting_shop_for_painting = 0
+                releaseassemblykits.paint_shop_for_assembly = 0
+                releaseassemblykits.assembly_shop = 0
+                releaseassemblykits.order = order
+
+            # Процент готовности заказа (данные монитора рабочих центров)
+            if orders_data[key].get('job monitor for work centers'):
+                monitor = JobMonitorForWorkCenters()
+                monitor.percentage_of_readiness_to_cut = orders_data[key]['job monitor for work centers']['percentage_of_readiness_to_cut']
+                monitor.number_of_details_plan = orders_data[key]['job monitor for work centers']['number_of_details_plan']
+                monitor.number_of_details_fact = orders_data[key]['job monitor for work centers']['number_of_details_fact']
+                monitor.order = order
+
+            # Данные о дополнительных заказах (раскрой на буфер и раскрой на прокраску)
+            if orders_data[key].get('child_orders'):
+                for key_child in orders_data[key]['child_orders']:
+                    child = ChildOrder()
+                    child.composite_key = key_child
+                    child.percentage_of_readiness_to_cut = orders_data[key]['child_orders'][key_child]['percentage_of_readiness_to_cut']
+                    child.number_of_details_plan = orders_data[key]['child_orders'][key_child]['number_of_details_plan']
+                    child.number_of_details_fact = orders_data[key]['child_orders'][key_child]['number_of_details_fact']
+                    child.order = order
+            session.add(order)
+    return session
--- extract_raw_data_from_excel_file_in_db.py
+++ extract_raw_data_from_excel_file_in_db.py
@@ -16,12 +16,11 @@ from openpyxl import load_workbook
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-from app import (ArticleExtractor, Base, DescriptionAdditionalOrderRowData,
-                 DescriptionMainOrderRowData, OrderRowData,
-                 ReleaseOfAssemblyKitsRowData, ReportSettingsOrders, JobMonitorForWorkCenters, ChildOrder,
+from app import (ArticleExtractor, Base, ReportSettingsOrders,
                  extract_data_job_monitor_for_work_centers,
                  extract_data_moving_sets_of_furniture,
-                 extract_data_release_of_assembly_kits)
+                 extract_data_release_of_assembly_kits,
+                 import_data_to_db_production_orders)
 
 config = ReportSettingsOrders()
 extractor = ArticleExtractor()
@@ -63,7 +62,7 @@ if __name__ == '__main__':
     )
 
     pprint(orders_data['202300920'])
-    
+
     # Создаем таблицы в БД
     engine = create_engine('sqlite:///data/orders_row_data.db')
     Base.metadata.drop_all(engine)
@@ -71,43 +70,7 @@ if __name__ == '__main__':
     session = Session(bind=engine)
 
     # Импортируем данные в БД
-    for key in orders_data:
-        if not (key == 'ErrorLog'):
-            order = OrderRowData()
-            order.composite_key = key
-            order.full_order_number = orders_data[key]['moving_sets_of_furniture']['full_order_number']
-            order.furniture_name = orders_data[key]['moving_sets_of_furniture']['furniture_name']
-            order.furniture_article = orders_data[key]['moving_sets_of_furniture']['furniture_article']
-            order.ordered = orders_data[key]['moving_sets_of_furniture']['ordered']
-            order.released = orders_data[key]['moving_sets_of_furniture']['released']
-            order.remains_to_release = orders_data[key]['moving_sets_of_furniture']['remains_to_release']
-            if orders_data[key].get("release_of_assembly_kits"):
-                releaseassemblykits = ReleaseOfAssemblyKitsRowData()
-                releaseassemblykits.cutting_shop_for_assembly = orders_data[key]['release_of_assembly_kits']['cutting_workshop_for_assembly']
-                releaseassemblykits.cutting_shop_for_painting = orders_data[key]['release_of_assembly_kits']['cutting_workshop_for_painting']
-                releaseassemblykits.paint_shop_for_assembly = orders_data[key]['release_of_assembly_kits']['paint_shop_for_assembly']
-                releaseassemblykits.assembly_shop = orders_data[key]['release_of_assembly_kits']['assembly_shop']
-                releaseassemblykits.order = order
-            else:
-                releaseassemblykits = ReleaseOfAssemblyKitsRowData()
-                releaseassemblykits.cutting_shop_for_assembly = 0
-                releaseassemblykits.cutting_shop_for_painting = 0
-                releaseassemblykits.paint_shop_for_assembly = 0
-                releaseassemblykits.assembly_shop = 0
-                releaseassemblykits.order = order
-            if orders_data[key].get('job monitor for work centers'):
-                monitor = JobMonitorForWorkCenters()
-                monitor.percentage_of_readiness_to_cut = orders_data[key]['job monitor for work centers']['percentage_of_readiness_to_cut']
-                monitor.number_of_details_plan = orders_data[key]['job monitor for work centers']['number_of_details_plan']
-                monitor.number_of_details_fact = orders_data[key]['job monitor for work centers']['number_of_details_fact']
-                monitor.order = order
-            if orders_data[key].get('child_orders'):
-                for key_child in orders_data[key]['child_orders']:
-                    child = ChildOrder()
-                    child.composite_key = key_child
-                    child.percentage_of_readiness_to_cut = orders_data[key]['child_orders'][key_child]['percentage_of_readiness_to_cut']
-                    child.number_of_details_plan = orders_data[key]['child_orders'][key_child]['number_of_details_plan']
-                    child.number_of_details_fact = orders_data[key]['child_orders'][key_child]['number_of_details_fact']
-                    child.order = order
-            session.add(order)
+    session = import_data_to_db_production_orders(orders_data=orders_data,
+                                                  session=session)
+    # Сохраняем таблицы в БД
     session.commit()
